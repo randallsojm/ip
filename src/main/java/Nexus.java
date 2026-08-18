9ce98cf59d39afb1ffbabd89dc0cb44e96f4a5e8
randallsojm <randallsojm@gmail.com>
2026-08-18 22:12:28 +0800

    Add initial greeting and exit

--- src/main/java/Nexus.java
+++ src/main/java/Nexus.java
@@ -1,10 +1,15 @@
 public class Nexus {
     public static void main(String[] args) {
-        String banner = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        String banner = " _   _  _____  __  __  _   _  _____\n"
+                + "| \\ | || ____| \\ \\/ / | | | ||  ___|\n"
+                + "|  \\| ||  _|    \\  /  | | | || |___ \n"
+                + "| |\\  || |___   /  \\  | |_| | ___| |\n"
+                + "|_| \\_||_____| /_/\\_\\  \\___/ |____/ ";
         System.out.println(banner);
+        System.out.println("Hello! I'm Nexus.");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
     }
 }
