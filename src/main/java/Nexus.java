e91b56e3dc4a1f25a885aa1f0ae39d3c0ac85447
randallsojm <randallsojm@gmail.com>
2026-08-18 22:44:06 +0800

    Add support for echo and bye

--- src/main/java/Nexus.java
+++ src/main/java/Nexus.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class Nexus {
     public static void main(String[] args) {
+        String line = "____________________________________________________________";
         String banner = " _   _  _____  __  __  _   _  _____\n"
                 + "| \\ | || ____| \\ \\/ / | | | ||  ___|\n"
                 + "|  \\| ||  _|    \\  /  | | | || |___ \n"
@@ -8,8 +11,22 @@ public class Nexus {
         System.out.println(banner);
         System.out.println("Hello! I'm Nexus.");
         System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        System.out.println(line);
+
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String command = scanner.nextLine();
+
+            if (command.equals("bye")) {
+                System.out.println(line);
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(line);
+                break;
+            }
+            System.out.println(line);
+            System.out.println(command);
+            System.out.println(line);
+        }
+        scanner.close();
     }
 }
