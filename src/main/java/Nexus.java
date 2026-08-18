9ca88cb1b6d26620e8a401e08f18a5b90a6008a0
randallsojm <randallsojm@gmail.com>
2026-08-18 21:54:31 +0800

    Rename chatbot from Duke to Nexus

--- src/main/java/Nexus.java
+++ src/main/java/Nexus.java
@@ -1,4 +1,4 @@
-public class Duke {
+public class Nexus {
     public static void main(String[] args) {
         String banner = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
