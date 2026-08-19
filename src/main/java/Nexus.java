83d05144467a9d432d0e8fb3a82aacbf26dcc3e0
randallsojm <randallsojm@gmail.com>
2026-08-19 17:17:37 +0800

    Add support for listing tasks in Nexus.java

--- src/main/java/Nexus.java
+++ src/main/java/Nexus.java
@@ -1,8 +1,10 @@
 import java.util.Scanner;
 
 public class Nexus {
+    private static final int MAX_TASKS = 100;
+    private static final String LINE = "____________________________________________________________";
+
     public static void main(String[] args) {
-        String line = "____________________________________________________________";
         String banner = " _   _  _____  __  __  _   _  _____\n"
                 + "| \\ | || ____| \\ \\/ / | | | ||  ___|\n"
                 + "|  \\| ||  _|    \\  /  | | | || |___ \n"
@@ -11,21 +13,35 @@ public class Nexus {
         System.out.println(banner);
         System.out.println("Hello! I'm Nexus.");
         System.out.println("What can I do for you?");
-        System.out.println(line);
+        System.out.println(LINE);
 
         Scanner scanner = new Scanner(System.in);
+        String[] tasks = new String[MAX_TASKS];
+        int taskCount = 0;
+
         while (scanner.hasNextLine()) {
             String command = scanner.nextLine();
 
             if (command.equals("bye")) {
-                System.out.println(line);
+                System.out.println(LINE);
                 System.out.println("Bye. Hope to see you again soon!");
-                System.out.println(line);
+                System.out.println(LINE);
                 break;
             }
-            System.out.println(line);
-            System.out.println(command);
-            System.out.println(line);
+
+            System.out.println(LINE);
+            if (command.equals("list")) {
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+            } else if (taskCount < MAX_TASKS) {
+                taskCount++;
+                tasks[taskCount] = command;
+                System.out.println("added: " + command);
+            } else {
+                System.out.println("Sorry, your task list is full.");
+            }
+            System.out.println(LINE);
         }
         scanner.close();
     }
